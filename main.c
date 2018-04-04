f11dbdb25e4abed95a12228f372140bf3d5d9d57
Franc[e]sco <lolisamurai@tfwno.gf>
2018-04-05 01:34:47 +0200

    expand case ranges for better compatibility with other compilers

--- main.c
+++ main.c
@@ -515,7 +515,8 @@ void lnext()
         lstring_literal();
         break;
 
-    case '0' ... '9':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
         for (; isdigit(*ldata); ++ldata);
 
         if (*ldata == '.' || tolower(*ldata) == 'e')
@@ -531,9 +532,14 @@ void lnext()
         }
         break;
 
-    case 'a' ... 'z':
-    case 'A' ... 'Z':
-    case '_':
+    case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
+    case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
+    case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
+    case 'v': case 'w': case 'x': case 'y': case 'z': case 'A': case 'B':
+    case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I':
+    case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P':
+    case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W':
+    case 'X': case 'Y': case 'Z': case '_':
         ltok.kind = TOKEN_NAME;
 
         while (isalpha(*ldata) || isdigit(*ldata) || *ldata == '_') {
